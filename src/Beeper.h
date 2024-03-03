55ecde7e021f9bc3f1e0cc0a340cbb2f7e7404c2
Luca Paolini <luca.paolini@fao.org>
2024-03-03 15:56:03 +0100

    Changed time sounds.

--- src/Beeper.h
+++ src/Beeper.h
@@ -33,8 +33,8 @@ class Beeper {
         Tone READY[4] = {{NOTE_C6, 100}, {NOTE_E6, 100}, {NOTE_G6, 100}, {0, 0}};
         Tone CLICK[2] = {{NOTE_C7, 25}, {0, 0}};
         Tone ALERT[3] = {{0, 500}, {NOTE_D6, 25}, {0, 0}};
-        Tone TIME_START[2] = {{NOTE_C7, 100}, {0, 0}};
-        Tone TIME_STOP[4] = {{NOTE_C7, 50}, {0, 50}, {NOTE_C7, 100}, {0, 0}};
+        Tone TIME_START[4] = {{NOTE_C7, 50}, {0, 50}, {NOTE_C7, 50}, {0, 0}};
+        Tone TIME_STOP[2] = {{NOTE_C7, 250}, {0, 0}};
         Tone LAST_TWO_MINUTES[2] = {{NOTE_C7, 250}, {0, 0}};
         Tone CONFIRM[4] = {{NOTE_B6, 50}, {0, 25}, {NOTE_D7, 50}, {0, 0}};
         Tone NOT_ALLOWED[2] = {{NOTE_C6, 250}, {0, 0}};
